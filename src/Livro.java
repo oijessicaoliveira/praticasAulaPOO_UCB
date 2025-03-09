c5ac700260c23babe128f81d067630fd821005c5
Jessica Oliveira <eijessicasousa@gmail.com>
2025-03-09 13:23:19 -0300

    Ficha literária completa.

--- src/Livro.java
+++ src/Livro.java
@@ -12,6 +12,9 @@ public class Livro {
         System.out.println("Nome do livro: " + nome);
         System.out.println("Nome do autor (a): " + autor);
         System.out.println("Editora: " + editora);
+        System.out.println("Número de páginas: " + numeroDePaginas);
+        System.out.println("Lido? " + lido);
+        System.out.println("Avaliação: " + avaliacao);
     }
 
 }
